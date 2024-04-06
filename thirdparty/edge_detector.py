4ce34bf468f20821969287d8901bfddec0a0d6fd
Marco <mv320@ic.ac.uk>
2024-04-06 12:13:07 +0100

    Fixed bug in edge_detector

--- thirdparty/edge_detector.py
+++ thirdparty/edge_detector.py
@@ -1,6 +1,6 @@
 import kornia as kn
 import argparse
-from thirdparty.pidinet import models
+from pidinet import models
 import torch
 from dataclasses import dataclass
 
@@ -15,13 +15,28 @@ def get_edge(img):
 @dataclass
 class PidinetConfig:
     pidinet_model: str = 'pidinet_tiny'
-    sa: bool = False  # Note: store_false means the default is True, but for clarity in dataclasses, we'll invert it and handle the logic in code.
-    dil: bool = False  # Similar to 'sa', handle the inversion in your application logic.
+    sa: bool = True  # Note: store_false means the default is True, but for clarity in dataclasses, we'll invert it and handle the logic in code.
+    dil: bool = True  # Similar to 'sa', handle the inversion in your application logic.
     config: str = 'carv4'
     evaluate: str = "thirdparty/pidinet/trained_models/table5_pidinet-tiny.pth"
-    gpu: str = '0'
-
+    gpu: int = 0
 pidnet_args = PidinetConfig() # NOTE: I changed it to a dataclass because the parser was creating conflicts
+
+# '''PidiNet egge detector'''
+# parser = argparse.ArgumentParser(description='PyTorch Pixel Difference Convolutional Networks')
+# parser.add_argument('--pidinet_model', type=str, default='pidinet_tiny',
+#         help='model to train the dataset')
+# parser.add_argument('--sa', action='store_false',
+#         help='use CSAM in pidinet')
+# parser.add_argument('--dil', action='store_false',
+#         help='use CDCM in pidinet')
+# parser.add_argument('--config', type=str, default='carv4',
+#         help='model configurations, please refer to models/config.py for possible configurations')
+# parser.add_argument('--evaluate', type=str, default="thirdparty/pidinet/trained_models/table5_pidinet-tiny.pth",
+#         help='full path to checkpoint to be evaluated')
+# parser.add_argument('--gpu', type=str, default='0', help='gpus available')
+
+# pidnet_args_2 = parser.parse_args()
 
 def Initialize_PidNet(args):
     ### Create model
@@ -41,3 +56,6 @@ def PidNet(model,img):
         results = model(img)
         result = results[-1]
     return result
+
+# Calculate the original's edge map.
+pidnet = Initialize_PidNet(pidnet_args)
